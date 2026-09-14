b8082813fad8d7ada23a3e79173f62511cf47827
jarrett197 <Jstephens0914@gmail.com>
2026-09-14 16:26:42 -0400

    Assignment 1 - Program 1 and Program 2

--- program-1/Assignment1Program1.java
+++ program-1/Assignment1Program1.java
@@ -1,5 +1,38 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Assignment1Program1 {
   public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    Random random = new Random();
 
+    // print my initials in block letters
+    System.out.println("JJJJJJJ    SSSSSSS");
+    System.out.println("   JJJ    SSS   SSS");
+    System.out.println("   JJJ    SSS");
+    System.out.println("   JJJ     SSSSSSS");
+    System.out.println("   JJJ          SSS");
+    System.out.println("JJ JJJ    SSS   SSS");
+    System.out.println(" JJJJ      SSSSSSS");
+    System.out.println();
+
+    // get a 5 character string and reverse the middle 3 characters
+    System.out.println("Please enter a 5-character string:");
+    String word = input.nextLine();
+    String middle = "" + word.charAt(3) + word.charAt(2) + word.charAt(1);
+    System.out.println();
+
+    // convert fahrenheit to celsius
+    System.out.println("Please enter a number in Fahrenheit:");
+    double fahrenheit = input.nextDouble();
+    double celsius = (fahrenheit - 32) * 5 / 9;
+    System.out.println();
+
+    // random number between 32 and 16384
+    int randomNumber = random.nextInt(16353) + 32;
+    System.out.println("Random number generated. Continuing...");
+    System.out.println();
+
+    System.out.println("Your new string is " + celsius + middle + randomNumber);
   }
 }
--- program-2/Assignment1Program2.java
+++ program-2/Assignment1Program2.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class Assignment1Program2 {
   public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
 
+    System.out.println("Base Conversion Program");
+    System.out.println();
+
+    System.out.print("Please enter a base (2 – 9): ");
+    int base = input.nextInt();
+    System.out.println();
+
+    // the biggest 4 digit number in a base is base^4 - 1
+    int max = (int) Math.pow(base, 4) - 1;
+
+    System.out.println("The maximum, 4-digit, base 10 number in base " + base + " is " + max + ".");
+    System.out.print("Now, enter a base 10 number in the range 0 to " + max + " to convert: ");
+    int number = input.nextInt();
+    System.out.println();
+
+    // split the number into 4 digits in the new base
+    int digit1 = number / (int) Math.pow(base, 3);
+    int leftOver = number % (int) Math.pow(base, 3);
+    int digit2 = leftOver / (int) Math.pow(base, 2);
+    leftOver = leftOver % (int) Math.pow(base, 2);
+    int digit3 = leftOver / base;
+    int digit4 = leftOver % base;
+
+    System.out.println(number + " (base 10) = " + digit1 + digit2 + digit3 + digit4 + " (base " + base + ")");
   }
 }
